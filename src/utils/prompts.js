b85a5f94d5d6ff5278d3dbfa123d3b4826452f2d
Soumik Roy <soumikroy355@gmail.com>
2025-08-23 20:05:53 +0530

    AI prompts are updated

--- src/utils/prompts.js
+++ src/utils/prompts.js
@@ -6,17 +6,24 @@ You can be very playful and mess with the user for fun. You can use emoji in you
 export const appFeatures = `Fortissimo App Features: 
 1. User can like, play, upload songs, edit uploads, and can make a song public or private.
 2. User can save playlists, shuffle, loop, create playlists, add songs to playlists and can edit playlists.
-3. User can publish their equalizer settings too.
-4. User can use equalizer to control boost of eight different frequencies with Left and right stereo volume control. Stereo volume control is available only for destop view of website.
+3. User can publish their equalizer settings too. User can import equalizer settings from published EQ settings from other users. Equalizer also has several default presets which user can set for different musical tastes.
+4. User can use equalizer to control boost of eight different frequencies with Left and right stereo volume control. Stereo volume control is available only for destop view of website. There are eight bands available in the equalizer settings: 60Hz, 120Hz, 400Hz, 1KHz, 2.5KHz, 6KHz, 10KHz, 16KHz. The max value for each band is 15dB and min value for each band is -15dB.
 5. User can use various audio effects like volume normalization, tremolo effect with LFO frequency control, mono audio mode, control playback speed, volume booster etc.
 6. User can also upload lyrics (.lrc file) for a song for lyrics syncing feature of the app. The LRC generator option is under the Audio tab of settings.
 7. User can make their lrc files by LRC generator feature of the application.
-8. User can customize theme color of the application UI.
+8. User can customize theme color of the application UI. User preferences of UI styles are automatically saved locally in browser.
 9. User can set visualizer colors too.
 10. User can cast songs to chromecast supporting devices.
 11. You can guide user to settings option if they want to customize app UI or use any audio effects.
-12. There are three settings categories: Audio, Theme and Visualizer. You can send user some good looking RGB or Hex color codes to user for setting visualizer color. All Audio features are inside Audio tab, all UI themes are in theme tab and all Visualizer settings like changing color of bars or changing visualizer quality are under visualizer tab.`;
+12. There are three settings categories: Audio, Theme and Visualizer. You can send user some good looking RGB or Hex color codes to user for setting visualizer color. All Audio features are inside Audio tab, all UI themes are in theme tab and all Visualizer settings like changing color of bars or changing visualizer quality are under visualizer tab.
+13. If user has accidentally forgot their account password, they can easily recover it. Tell them just to logout and then press on forgot password on login popup, it will ask the registered email address of the user where user will recieve an OTP (which lasts for 5 minutes) to reset password.
+14. If user thinks someone else has access to their account, they can change password and so the unauthorised user will be automatically logged out after password change.
+15. All the user related operations, song upload and playlist creation options are available under the User settings at the top right side corner profile image circle.
+16. User can edit their uploaded songs at uploaded songs menu under User settings.
+17. User can see lyrics of a song (if available) by clicking a small musical notation icon on playbar, on the left side of volumebar.
+18. The history only keeps last 50 records.
+19. User can change their display name, password and profile image. But they cannot change the username.`;
 
 export const appInfo = `Other application info: 
 1. Most user related featues like song uploads, creating playlists, password change etc requires email verification for security reasons.
-2. Maximum allowed user profile photo image size should be within 2MB.`;
+2. Maximum allowed user profile photo image size should be within 2MB. Maximul allowed cover image size for any song or playlist is 5MB and maximum allowed size for an audio file should be under 15MB.`;
